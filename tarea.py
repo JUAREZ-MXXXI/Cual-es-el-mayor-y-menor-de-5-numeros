3327154a548abb44b188097b044945d9ceb9612c
JUAREZ-MXXXI <138530165+JUAREZ-MXXXI@users.noreply.github.com>
2023-07-05 18:23:50 -0500

    Add files via upload

---
+++ tarea.py
@@ -0,0 +1,11 @@
+# ingresar 5 numeros  y detectar el mayor y menor
+
+a1 = int(input("Ingrese el primer numero: "))
+b2 = int(input("Ingrese el segundo numero: "))
+c3 = int(input("Ingrese el tercero numero: "))
+d4 = int(input("Ingrese el cuarto numero: "))
+e5 = int(input("Ingrese el quinto numero: "))
+
+
+print("Elnumero mayor es: ",max(a1,b2,c3,d4,e5))
+print("Elnumero menor es: ",min(a1,b2,c3,d4,e5))
